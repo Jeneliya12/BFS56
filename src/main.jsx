c0326e8bf5f814fa10a79a4a2120fc6a141c6b5f
Jeneliya12 <gurungjeneliya@gmail.com>
2024-07-27 18:22:06 -0400

    implement css box model

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,6 @@ import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <main className="container">
-      <section className="sectionone">
-        <div className="boxone">
-          <h1 className="headingone">Heading 1</h1>
-          <p className="paragraphone">Paragraph 1</p>
-        </div>
-      </section>
-      <section className="sectiontwo">
-        <div className="boxtwo">
-          <h1 className="headingtwo">Heading 2</h1>
-          <p className="paragraphtwo">Paragraph 2</p>
-        </div>
-      </section>
-    </main>
+    <div className="box">Hello</div>
   </React.StrictMode>
 );
