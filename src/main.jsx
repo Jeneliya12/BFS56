4d37ac91595d7a75a0ebf256d401edd81ba7c434
Jeneliya12 <gurungjeneliya@gmail.com>
2024-07-27 00:01:22 -0400

    adding html code in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,14 @@ import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <h1>Hello, I am Jeneliya.</h1>
+    <div> Hello </div>
+    <p>Paragraph</p>
+    <h1>Heading</h1>
+    <br />
+    <h1> New Heading </h1>
+    <img
+      src="https://images.pexels.com/photos/599708/pexels-photo-599708.jpeg?cs=srgb&dl=pexels-sunil-patel-191908-599708.jpg&fm=jpg"
+      alt="Giant Tree"
+    />
   </React.StrictMode>
 );
