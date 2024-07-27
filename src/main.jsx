2181ff7444e7d344aa21c5a2e84ae05e98871514
Jeneliya12 <gurungjeneliya@gmail.com>
2024-07-27 17:04:27 -0400

    jsx vs html

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello, I am Jeneliya.</h1>
-    <div> Hello </div>
-    <p>Paragraph</p>
-    <h1>Heading</h1>
-    <br />
-    <h1> New Heading </h1>
-    <img
-      src="https://images.pexels.com/photos/599708/pexels-photo-599708.jpeg?cs=srgb&dl=pexels-sunil-patel-191908-599708.jpg&fm=jpg"
-      alt="Giant Tree"
-    />
+    <form>
+      <label htmlFor="fname">First name:</label>
+      <br />
+      <input type="text" id="fname" name="fname" value="John" />
+      <br />
+      <label for="lname">Last name:</label>
+      <br />
+      <input type="text" id="lname" name="lname" value="Doe" />
+      <br />
+      <br />
+      <input type="submit" value="submit" />
+    </form>
   </React.StrictMode>
 );
