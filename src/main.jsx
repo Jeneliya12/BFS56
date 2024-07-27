0a038c5beb23c3e005afb14e5f8d2a95461d74cc
Jeneliya12 <gurungjeneliya@gmail.com>
2024-07-27 17:32:02 -0400

    apply different sections

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,19 @@ import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <form>
-      <label htmlFor="fname">First name:</label>
-      <br />
-      <input type="text" id="fname" name="fname" value="John" />
-      <br />
-      <label for="lname">Last name:</label>
-      <br />
-      <input type="text" id="lname" name="lname" value="Doe" />
-      <br />
-      <br />
-      <input type="submit" value="submit" />
-    </form>
+    <main className="container">
+      <section className="sectionone">
+        <div className="boxone">
+          <h1 className="headingone">Heading 1</h1>
+          <p className="paragraphone">Paragraph 1</p>
+        </div>
+      </section>
+      <section className="sectiontwo">
+        <div className="boxtwo">
+          <h1 className="headingtwo">Heading 2</h1>
+          <p className="paragraphtwo">Paragraph 2</p>
+        </div>
+      </section>
+    </main>
   </React.StrictMode>
 );
